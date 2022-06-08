97d5a4b21a199e1b7cad3b085475dce903ae4e94
SalMaciel <sal.macielm@gmail.com>
2022-06-07 23:07:05 -0700

    learning branches and merging

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 // Get data from cities.js
 let cityData = cities;
 // Loop through the cities array and create one marker for each city.
